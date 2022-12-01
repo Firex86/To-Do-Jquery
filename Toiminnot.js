ae5bcdf4530f40dbbaf2a438ca821d7cf2a6b596
Firex86 <46250017+Firex86@users.noreply.github.com>
2022-12-01 13:33:16 +0200

    Update Toiminnot.js

--- Toiminnot.js
+++ Toiminnot.js
@@ -1,81 +1,21 @@
-const kirjoitus = document.querySelector('.kirjoitusbox')
-const nappi = document.querySelector('.lisaanappi')
-const lista = document.querySelector('.todolista')
-const poistakaikki = document.querySelector('.poistakaikki')
-const valmiskaikki = document.querySelector('.valmiskaikki')
-
-function painaNappia(e) {
-    e.preventDefault()
-    LisääTodo()
-}
-
-// Listätään To Do listaan
-function LisääTodo() {
-    const kaikkipalat = document.createElement('div')
-    kaikkipalat.classList.add('kaikkipalat')
-
-    //Luodaan elementti p joka sisältää kirjoitetun ja lisätään pala elementtiin, joka osa kaikkipalat elementtiä
-    const pala = document.createElement('p')
-    pala.classList.add('pala')
-    pala.innerText = kirjoitus.value
-    kaikkipalat.appendChild(pala)
-
-    // Tarkistetaan onko tyhjä ja annetaan varoitus
-    if (kirjoitus.value === '') {
-        alert("Tekstikenttä on tyhjä! Et voi lisätä listausta.")
-    } else {
-
-    //Luodaan valmisbutton nappi
-    const valmisbutton = document.createElement("button")
-        valmisbutton.innerHTML = '<i class="buttontwo"></i>'
-        valmisbutton.classList.add("valmis-button")
-        kaikkipalat.appendChild(valmisbutton)
-
-    //Luodaan roskabutton nappi.
-    const roskabutton = document.createElement("button")
-        roskabutton.innerHTML = '<i class="buttontwo"></i>'
-        roskabutton.classList.add("roska-button")
-        kaikkipalat.appendChild(roskabutton)
-
-    lista.appendChild(kaikkipalat)
-    kirjoitus.value = ''
-    }
-
-    //Tallennus localStorageen
-    localStorage.setItem('Listani', pala.innerText);
-}
-
-// Valmis ja poista toiminnot
-function merkkaa(e) {
-    const pala = e.target
-
-    // Poista
-    if (pala.classList[0] === 'roska-button') {
-        const todolist = pala.parentElement
-        todolist.remove()
-    }
-    
-    // Valmis
-    if (pala.classList[0] === 'valmis-button') {
-        const todolist = pala.parentElement
-        todolist.classList.toggle('valmis')
-    }
-   
-    
-}
-
-// Poista kaikki listaukset
-    poistakaikki.onclick = () => {
-        document
-            .querySelectorAll(".kaikkipalat")
-            .forEach((e) => e.parentNode.removeChild(e));
-}
-
-    
-// Merkitse kaikki valmiiksi
-    valmiskaikki.onclick = () => {
-        document.querySelector(".todolista").classList.toggle('valmis');
-    }
-
-nappi.addEventListener('click', painaNappia)
-lista.addEventListener('click', merkkaa)
+$(document).ready(function() {
+var count = 0;
+    $(".add").click(function() {
+        var inputval = $(".form-control").val();
+        if (inputval != '') {
+            count ++;
+            $(".todo-list").append('<li><div class="left-cont"><input type="checkbox" id="check-'+count+'" name=""><label for="check-'+count+'"></label><span>'+inputval+'</span></div><span class="remove"> <i class="material-icons">delete</i> </span></li>');
+        }
+        $(".form-control").val('');
+    })
+    $(document).on('change', 'input[type="checkbox"]', function() {
+        if ($(this).is(':checked')) {
+            $(this).closest("li").children(".remove").addClass("active");
+        } else {
+            $(this).closest("li").children(".remove").removeClass("active");
+        }
+    })
+    $(document).on('click', '.remove', function() {
+        $(this).parent().remove();
+    })
+});
